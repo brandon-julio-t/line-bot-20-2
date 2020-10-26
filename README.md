cc68e3207e4bbc3c8cf34f5f8f04bc8fecd84b4a
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-10-26 22:10:24 +0700

    😈 trying to piss off anti-typescript people

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # line-bot-20-2
- LINE Bot for 20-2 Assistants
+
+LINE Bot for 20-2 Assistants
+
+## NPM
+
+1. `npm install`
+2. `npm start`
--- index.js
+++ index.js
@@ -19,12 +19,14 @@ cron.schedule('0 19 * * mon-fri', () => sendBroadcast('Shift malam clock out gen
 cron.schedule('0 13 * * sat', () => sendBroadcast('Shift pagi clock out gengs.'));
 cron.schedule('0 17 * * sat', () => sendBroadcast('Shift malam clock out gengs.'));
 cron.schedule('0 21 * * sat', () => sendBroadcast('Eval angkatan gengs.'));
-cron.schedule('* * * * sun', () => {
-  if (!hasSaidHappySunday) {
-    state.channel?.send(`Happy Sunday @everyone (yang bikin bot baru bangun).`);
-    hasSaidHappySunday = true;
-  }
-});
+cron.schedule(
+  '* * * * sun',
+  () =>
+    // buat yang bilang pake node.js tanpa typescript, selamat membaca code ini, 🥚.
+    !hasSaidHappySunday &&
+    (hasSaidHappySunday = true) &&
+    sendBroadcast('Happy Sunday @everyone (yang bikin bot baru bangun).')
+);
 
 async function sendBroadcast(text) {
   try {
